d2d1eff90dadc946e984ea5262758a937c1f255e
freshcoderchan <freshcoderjack@outlook.com>
2018-08-18 17:08:18 +0200

    Fix Relation - Blog can have several categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180817181820) do
+ActiveRecord::Schema.define(version: 20180818150339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
