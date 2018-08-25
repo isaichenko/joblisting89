16d9d144138f14c64cbcec3ca57d54d0a1f909cf
reebok159 <reebok159@bk.ru>
2018-08-25 17:22:31 +0300

    setting sendgrid provider, mail template

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,20 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: "support@joblisting88.com"
   layout 'mailer'
+  before_action :set_x_smtpapi
+
+  private
+
+  def set_x_smtpapi
+    headers 'X-SMTPAPI' => {
+      filters: {
+        templates: {
+          settings: {
+            enable: 1,
+            template_id: '74d9616d-13e5-457c-bad9-3a491e752c81'
+          }
+        }
+      }
+    }.to_json
+  end
 end
