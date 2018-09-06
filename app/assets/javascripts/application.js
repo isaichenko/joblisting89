ab49cdd9a38491dfe22bb0a77b8a2027f9bcdf05
isaichenko <isaichenko.e@gmail.com>
2018-09-06 17:03:51 +0300

    theme inplement 1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,7 +25,25 @@
 //= require select2-full
 //= require social-share-button
 
+//= require jquery-2.1.3.min
+//= require chosen.jquery.min
+//= require jquery.counterup.min
+//= require jquery.flexslider-min
+//= require jquery.gmaps.min
+//= require jquery.jpanelmenu
+//= require jquery.magnific-popup.min
+//= require jquery.sceditor.bbcode.min
+//= require jquery.sceditor
+//= require jquery.superfish
+//= require jquery.themepunch.revolution.min
+//= require jquery.themepunch.showbizpro.min
+//= require jquery.themepunch.tools.min
+//= require headroom.min
+//= require hoverIntent
+//= require slick.min
+//= require stacktable
+//= require switcher
+//= require waypoints.min
+//= require custom
+
 //= require_tree .
-
-
-
