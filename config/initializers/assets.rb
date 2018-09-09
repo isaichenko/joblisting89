8525202fcaa51d53fcbcfebdc6054ab397cb3deb
isaichenko <isaichenko.e@gmail.com>
2018-09-09 20:22:27 +0300

    index

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # ckeditor
-Rails.application.config.assets.precompile += %w( ckeditor/* )
+Rails.application.config.assets.precompile += %w( ckeditor/* colors/green )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
