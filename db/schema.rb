b5c95f2aeb6fade45c9a60335ddd224aead44f8d
isaichenko <isaichenko.e@gmail.com>
2018-09-11 13:05:30 +0300

    config elastic

--- db/schema.rb
+++ db/schema.rb
@@ -320,6 +320,7 @@ ActiveRecord::Schema.define(version: 20180825152129) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.integer "status", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "provider"
