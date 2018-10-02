35e3f22052cf7be9b7201b70322a912755118cbc
isaichenko <isaichenko.e@gmail.com>
2018-10-02 16:49:29 +0300

    banner-home and registration updates

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -8,5 +8,6 @@ module DeviseWhitelist
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:initial_interface])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:interface])
   end
 end
