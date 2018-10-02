c542347b52e7b603e011cbf4534f6effc80d0214
isaichenko <isaichenko.e@gmail.com>
2018-10-02 22:37:40 +0300

    5

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -116,8 +116,10 @@ class PagesController < ApplicationController
 
   def set_job_post
     @recent_jobs = Job.last(5)
-    @spotlight_jobs = Job.last(2)
+    @spotlight_jobs = []
     @spotlight_jobs << Job.find(16)
+    @spotlight_jobs << Job.find(12)
+    @spotlight_jobs << Job.find(11)
 
     @recent_posts = Blog.last(3)
   end
